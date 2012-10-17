7a655633ea45cbadcf7ed9a08c001b0230c8a66a
herbion <import.future@gmail.com>
2012-10-18 02:59:06 +0300

    Added log4j.xml

--- src/main/java/com/web/app/HomeController.java
+++ src/main/java/com/web/app/HomeController.java
@@ -1,14 +1,17 @@
 package com.web.app;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	private final static Logger log = Logger.getLogger(HomeController.class);
+	
 	@RequestMapping(value = "/")
 	public String home() {
 		//light comment		
-		System.out.println("HomeController: Passing through...");
+		log.info("HomeController: Passing through...");
 		return "WEB-INF/views/home.jsp";
 	}
 }
