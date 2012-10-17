8090ac27ef46f50ae99397a7046378da13e30991
herbion <import.future@gmail.com>
2012-10-17 16:40:56 +0300

    Addend comment

--- src/main/java/com/web/app/HomeController.java
+++ src/main/java/com/web/app/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	@RequestMapping(value = "/")
 	public String home() {
+		//light comment		
 		System.out.println("HomeController: Passing through...");
 		return "WEB-INF/views/home.jsp";
 	}
