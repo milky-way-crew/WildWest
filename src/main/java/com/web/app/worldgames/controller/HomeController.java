9b859f089b816a9cfe6c7f95bc4703b334f070f9
herbion <import.future@gmail.com>
2012-10-23 17:23:37 +0300

    Home controller fix

--- src/main/java/com/web/app/worldgames/controller/HomeController.java
+++ src/main/java/com/web/app/worldgames/controller/HomeController.java
@@ -1,16 +1,16 @@
-package com.web.app;
+package com.web.app.worldgames.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller()
+@Controller
 public class HomeController {
 	private final static Logger log = Logger.getLogger(HomeController.class);
-	
-	@RequestMapping(value = "/")
+
+	@RequestMapping(value = { "/", "/home" })
 	public String home() {
-		//light comment		
+		// light comment
 		log.info("HomeController: Passing through...");
 		return "home";
 	}
