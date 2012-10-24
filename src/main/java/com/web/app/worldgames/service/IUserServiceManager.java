a56e0d20ad3fa07fd91e93cdbe89f2f1d7ce4dd3
DancerInTheDark <palach.t@gmail.com>
2012-10-24 17:21:33 +0300

    Wrong method!

--- src/main/java/com/web/app/worldgames/service/IUserServiceManager.java
+++ src/main/java/com/web/app/worldgames/service/IUserServiceManager.java
@@ -3,7 +3,7 @@ package com.web.app.worldgames.service;
 import com.web.app.worldgames.domain.User;
 
 public interface IUserServiceManager {
-    User findUserByLogin(String email);
+    User findUserByLogin(String login);
     User findUserByNickname(String nickname);
     User findUserByEmail(String email);
     User logInUser(String login, String password);
