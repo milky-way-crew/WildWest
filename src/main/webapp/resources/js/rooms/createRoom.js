2e8e91c343c2bb6641be516012e702691f07df90
LittleInn <InnaPetryshyn@gmail.com>
2012-10-26 15:42:04 +0300

    add .js

---
+++ src/main/webapp/resources/js/rooms/createRoom.js
@@ -0,0 +1,13 @@
+var myselect = document.getElementById('rooms');
+
+
+function createOption(){
+    var currentText = document.getElementById('roomName').value;
+    var objOption = document.createElement("option");
+    objOption.text = currentText ;
+    objOption.value = currentText ;
+    myselect.options.add(objOption);
+}
+
+
+document.getElementById('createRoom').onclick = createOption;
