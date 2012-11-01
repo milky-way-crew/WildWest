65a0d635434d4ddd37dea5053bb8a69b2d87e5f8
herbion <import.future@gmail.com>
2012-11-01 18:19:30 +0200

    Working on js

--- src/main/webapp/resources/js/chess.js
+++ src/main/webapp/resources/js/chess.js
@@ -14,7 +14,8 @@ var GAME = {
 	options : {
 		ajax_url : 'chess2',
 		owner : 1,
-		board : new Object()
+		board : new Object(),
+		can_move : true
 	},
 
 	sendMessage : function(data, callback) {
@@ -30,7 +31,6 @@ var GAME = {
 	initBoard : function() {
 		this.sendMessage({}, function(json) {
 			var board = json.board;
-			// GAME.options.board = board;
 
 			for ( var i = 0; i < board.length; i++) {
 				var cell = board[i];
@@ -46,60 +46,125 @@ var GAME = {
 					$('#' + key + ' a').html('?');
 				}
 			}
-
-			// for ( var i = 0; i < board.length; i++) {
-			// var cell = board[i];
-			// if (cell.type != null) {
-			// var id = "#" + cell.position.x + "" + cell.position.y;
-			// var img = cell.type[0];
-			// // GAME.options.board[cell.position.x + "" +
-			// // cell.position.y] = {
-			// //
-			// // owner : cell.owner,
-			// // type : cell.type
-			// //
-			// // };
-			// if (cell.owner === null) {
-			// // pass
-			// } else if (cell.owner.id != GAME.options.owner) {
-			// $cell = $(id + ' a');
-			// $cell.html("?");
-			// } else {
-			// $cell = $(id + ' a');
-			// $cell.html(img);
-			// // $($cell.find('a')).addClass('w');
-			// }
-			// }
-			// }
 		});
 	},
+
 	makeMove : function(from_id, to_id) {
-		var gameBoard = GAME.options.board;
-		
-		if (gameBoard[to_id].owner == null) {
-			console.log('MAKE_MOVE: empty cell');
-			var copy = gameBoard[from_id];
-			gameBoard[from_id] = {
-					type : null,
-					owner : null
-			};
-			gameBoard[to_id] = copy;
+		var GAMEBoard = GAME.options.board;
+		var emptyCell = {
+			type : null,
+			owner : null,
+		};
+		if (GAMEBoard[to_id].owner == null) {
+			// Its ok, empty cell
+			console.log('MAKE_MOVE: Empty cell');
+			var copy = GAMEBoard[from_id];
+			GAMEBoard[from_id] = emptyCell;
+			GAMEBoard[to_id] = copy;
+			return GAME.view.animateMove(from_id, to_id);
+
 		} else {
-			var copy = gameBoard[from_id];
-			gameBoard[from_id] = {
-					type : null,
-					owner : null
-			};
-			gameBoard[to_id] = copy;
+			// Its opponent cell
+			console.log('MAKE_MOVE: Opponent cell');
+
+			var battleResult = GAME.battle(from_id, to_id);
+			// sendMessage( { MOVE }, function(data) onSucces {
+			// 	LOOSE, DRAW, WIN
+			// }  )
+
+			console.log('RESULT: ' + battleResult);
+
+			if (battleResult === "WIN") {
+				var copy = GAMEBoard[from_id];
+				GAMEBoard[from_id] = emptyCell;
+				GAMEBoard[to_id] = copy;
+
+				GAME.view.animateWin(from_id, to_id);
+				return;
+			} else if (battleResult === "LOOSE") {
+				GAMEBoard[from_id] = emptyCell;
+				// SHOW TYPE OF FIGURE DONT FORGET
+				GAME.view.animateLoose(from_id, to_id);
+				return;
+			} else if (battleResult === "DRAW") {
+				GAME.view.animateDraw(from_id, to_id);
+			} else {
+				console.error("*** Unknown battle result");
+			}
 		}
 	},
 
 	isOwnerOf : function(id) {
-		console.log("OWNER OF " + id + " is "
+		console
+				.log("OWNER OF " + id + " is "
 						+ GAME.options.board[id].owner.id);
 		console.log("PLAYER OWNER ID: " + GAME.options.owner);
 
 		return GAME.options.board[id].owner.id == GAME.options.owner;
+	},
+
+	battle : function(id_from, id_to) {
+		return "LOOSE";
+		// return "WIN";
+	},
+
+	view : {
+		animateMove : function(from_id, to_id) {
+			$('.selected').fadeOut(500, function() {
+				var $from = $('#' + from_id);
+				var $to = $('#' + to_id);
+				var $figure = $('.selected');
+
+				$from.effect("highlight", {}, 1000);
+				$to.html($('.selected'));
+				$figure.fadeIn(500, function() {
+					$to.effect("highlight", {}, 1000);
+				});
+				$figure.removeClass('selected');
+			});
+		},
+		animateLoose : function(from_id, to_id) {
+			var $from = $('#' + from_id);
+			var $to = $('#' + to_id);
+
+			$from.find('.figure').fadeOut(1000);
+			$from.html('');
+			$to.find('.figure').effect("bounce", {
+				times : 3
+			}, 1000);
+			// THATS MUST BE NOT HERE
+			$('#' + to_id + ' a').html(GAME.options.board[to_id].type[0]);
+		},
+		animateWin : function(from_id, to_id) {
+			var $to = $('#' + to_id);
+			var $from_figure = $('.selected');
+			var $to_figure = $($to.find('a'));
+
+			$from_figure.fadeOut(500);
+			$to_figure.fadeOut(500, function() {
+				$to.html($from_figure);
+				$from_figure.fadeIn(500);
+				$from_figure.removeClass('.selected');
+			});
+		},
+		animateDraw : null,
+	},
+
+	isValid : function(from, to) {
+		// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		// FIXME
+		// return true;
+		var xDelta = Math.abs(from[0] - to[0]);
+		var yDelta = Math.abs(from[1] - to[1]);
+
+		if (yDelta > 1 || xDelta > 1) {
+			return false; // Moves on distant >1 cells not allowed
+		} else if (yDelta - xDelta == 0) {
+			return false; // Diagonal moves not allowed
+		} else {
+			return true; // Everything is OK.
+		}
+		return true;
 	}
 
 };
@@ -112,29 +177,10 @@ $(document).ready(function() {
 		height : board.height()
 	};
 
-	var Util = {
-		isValid : function(from, to) {
-			// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-			// FIXME
-			return true;
-			var xDelta = Math.abs(from[0] - to[0]);
-			var yDelta = Math.abs(from[1] - to[1]);
-
-			if (yDelta > 1 || xDelta > 1) {
-				return false; // Moves on distant >1 cells not allowed
-			} else if (yDelta - xDelta == 0) {
-				return false; // Diagonal moves not allowed
-			} else {
-				return true; // Everything is OK.
-			}
-			return true;
-		}
-	};
-
 	var hoverIn = function() {
 		if ($('.selected').length > 0) {
 			var from = $('.selected').parent().attr('id');
-			if (Util.isValid(from, $(this).attr('id'))) {
+			if (GAME.isValid(from, $(this).attr('id'))) {
 				$(this).addClass('valid');
 			} else {
 				$(this).addClass('invalid');
@@ -151,6 +197,8 @@ $(document).ready(function() {
 		$(this).hover(hoverIn, hoverOut);
 	});
 
+	var lock = false;
+
 	$('#chess_board td').each(function() {
 		$(this).click(function() {
 			if ($('.selected').length > 0) {
@@ -160,22 +208,10 @@ $(document).ready(function() {
 				var id_from = $fromCell.attr('id');
 				var id_clicked = $toCell.attr('id');
 
-				console.log('Empty cell');
-
 				console.log(id_from);
 				console.log(id_clicked);
 				GAME.makeMove(id_from, id_clicked);
-
-				$('.selected').fadeOut(500, function() {
-					$($fromCell).effect("highlight", {}, 1000);
-					$toCell.html($('.selected'));
-					$('.selected').fadeIn(500, function() {
-						$toCell.effect("highlight", {}, 1000);
-					});
-					$('.selected').removeClass('selected');
-				});
 			}
-
 		});
 	});
 
@@ -192,7 +228,7 @@ $(document).ready(function() {
 				}, 300);
 				return false;
 			} else {
-				
+
 			}
 		});
 	});
