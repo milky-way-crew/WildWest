8d1ea5ab8ada75bb0616095ad8cad41e47600e03
LittleInn <InnaPetryshyn@gmail.com>
2012-11-14 14:20:22 +0200

    add buttonsLabel

---
+++ src/main/java/com/web/app/worldgames/domain/monopoly/ButtonsLabel.java
@@ -0,0 +1,15 @@
+package com.web.app.worldgames.domain.monopoly;
+
+public interface ButtonsLabel {
+	String ROLL = "roll";
+	String BUY = "buy";
+	String BUILD = "build";
+	String OK = "ok";
+	String REFUSE = "refuse";
+	String PAY = "pay";
+	String MORTAGE = "mortage";
+	String UNMORTAGE = "unmortage";
+	String SELL = "sell";
+	String START = "start";
+	String READY = "ready";
+}
