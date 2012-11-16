290bf5baab3d87a1cca4ec5c1201a168fe3193c5
herbion <import.future@gmail.com>
2012-11-16 20:41:45 +0200

    ',...,'

--- src/main/webapp/resources/js/board/gameBoard.js
+++ src/main/webapp/resources/js/board/gameBoard.js
@@ -1,288 +1,260 @@
- function selected(){
- alert('Selected');
- };
+function selected() {
+	alert('Selected');
+};
 
-$(document).ready(function(){
-alert("Hello");
+// $(document).ready(
 
-var DICE1=3;
-var DICE2=2;
+// function() {
+	alert("Hello");
 
-var DICE = DICE1+DICE2;
-var JDICE = 1;
-var start = 1;
+	var DICE1 = 3;
+	var DICE2 = 2;
 
-var PLAYER_1 = '#player1';
-var PLAYER_2 = '#player2';
-var PLAYER_3 = '#player3';
-var PLAYER_4 = '#player4';
+	var DICE = DICE1 + DICE2;
+	var JDICE = 1;
+	var start = 1;
 
-var DURATION = 500;
+	var PLAYER_1 = '#player1';
+	var PLAYER_2 = '#player2';
+	var PLAYER_3 = '#player3';
+	var PLAYER_4 = '#player4';
 
-/* Value of shift in MOVE methods */
-var LEFT_BIG_SHIFT = '-=70%';
-var LEFT_SMALL_SHIFT = '-=51%';
-var TOP_BIG_SHIFT = '-=550%';
-var TOP_SMALL_SHIFT = '-=370%';
-var RIGHT_BIG_SHIFT = '+=70%';
-var RIGHT_SMALL_SHIFT = '+=51%';
-var DOWN_BIG_SHIFT = '+=550%';
-var DOWN_SMALL_SHIFT = '+=370%';
+	var DURATION = 500;
 
-/* Value of shift in JUMP methods */
-var LEFT_BIG_JUMP = '-=100%';
-var LEFT_SMALL_JUMP = '-=74%';
-var TOP_BIG_JUMP = '-=110%';
-var TOP_SMALL_JUMP = '-=74%';
-var RIGHT_BIG_JUMP = '+=100%';
-var RIGHT_SMALL_JUMP = '+=74%';
-var DOWN_BIG_JUMP = '+=110%';
-var DOWN_SMALL_JUMP = '+=74%';
+	/* Value of shift in MOVE methods */
+	var LEFT_BIG_SHIFT = '-=70%';
+	var LEFT_SMALL_SHIFT = '-=51%';
+	var TOP_BIG_SHIFT = '-=550%';
+	var TOP_SMALL_SHIFT = '-=370%';
+	var RIGHT_BIG_SHIFT = '+=70%';
+	var RIGHT_SMALL_SHIFT = '+=51%';
+	var DOWN_BIG_SHIFT = '+=550%';
+	var DOWN_SMALL_SHIFT = '+=370%';
 
-/*  Color of player */
+	/* Value of shift in JUMP methods */
+	var LEFT_BIG_JUMP = '-=100%';
+	var LEFT_SMALL_JUMP = '-=74%';
+	var TOP_BIG_JUMP = '-=110%';
+	var TOP_SMALL_JUMP = '-=74%';
+	var RIGHT_BIG_JUMP = '+=100%';
+	var RIGHT_SMALL_JUMP = '+=74%';
+	var DOWN_BIG_JUMP = '+=110%';
+	var DOWN_SMALL_JUMP = '+=74%';
 
-var COLOR_PLAYER_1 = "brown";
-var COLOR_PLAYER_2 = "green";
-var COLOR_PLAYER_3 = "red";
-var COLOR_PLAYER_4 = "violer";
+	/* Color of player */
 
+	var COLOR_PLAYER_1 = "brown";
+	var COLOR_PLAYER_2 = "green";
+	var COLOR_PLAYER_3 = "red";
+	var COLOR_PLAYER_4 = "violer";
 
-	function playerLeftAnimate(player, shift){
-	$(player).animate({left : shift}, DURATION);
+	function playerLeftAnimate(player, shift) {
+		$(player).animate({
+			left: shift
+		}, DURATION);
 	}
-	
-	function playerTopAnimate(player, shift){
-	$(player).animate({top : shift}, DURATION);
+
+	function playerTopAnimate(player, shift) {
+		$(player).animate({
+			top: shift
+		}, DURATION);
 	}
-
-
 
 	/* Move the player */
-	function stepOnBoard(player){
-		for (var i = 0; i<DICE; i++){
-			if(start>=1 && start<11){
-				if(start==1){
+
+	function stepOnBoard(player) {
+		for(var i = 0; i < DICE; i++) {
+			if(start >= 1 && start < 11) {
+				if(start == 1) {
 					playerLeftAnimate(player, LEFT_BIG_SHIFT);
-				}else if(start==10){
+				} else if(start == 10) {
 					playerLeftAnimate(player, LEFT_BIG_SHIFT);
-				}else{
+				} else {
 					playerLeftAnimate(player, LEFT_SMALL_SHIFT);
 				}
-			}else if(start>=11 && start<21){
-				if(start==11){
+			} else if(start >= 11 && start < 21) {
+				if(start == 11) {
 					playerTopAnimate(player, TOP_BIG_SHIFT);
-				}else if(start==20){
+				} else if(start == 20) {
 					playerTopAnimate(player, TOP_BIG_SHIFT);
-				}else{
+				} else {
 					playerTopAnimate(player, TOP_SMALL_SHIFT);
 				}
 
-			}else if(start>=21 && start<31){
-				if(start==21){
+			} else if(start >= 21 && start < 31) {
+				if(start == 21) {
 					playerLeftAnimate(player, RIGHT_BIG_SHIFT);
-				}else if(start==30){
+				} else if(start == 30) {
 					playerLeftAnimate(player, RIGHT_BIG_SHIFT);
-				}else{
+				} else {
 					playerLeftAnimate(player, RIGHT_SMALL_SHIFT);
 				}
-			}else{
-				if(start==31){
+			} else {
+				if(start == 31) {
 					playerTopAnimate(player, DOWN_BIG_SHIFT);
-				}else if(start==40){
+				} else if(start == 40) {
 					playerTopAnimate(player, DOWN_BIG_SHIFT);
-				}else{
+				} else {
 					playerTopAnimate(player, DOWN_SMALL_SHIFT);
 				}
 			}
 
-		start++;
-		if(start>40)
-		start-=40;
+			start++;
+			if(start > 40) start -= 40;
 		}
 	}
-	
-		function playerLeftJump(player, shift){
-	$(player).css({left : shift});
-	}
-	
-	function playerTopJump(player, shift){
-	$(player).css({top : shift});
-	}
-	
-	
 
+	function playerLeftJump(player, shift) {
+		$(player).css({
+			left: shift
+		});
+	}
 
-	
-	
+	function playerTopJump(player, shift) {
+		$(player).css({
+			top: shift
+		});
+	}
+
 	/* JUMP the player */
-	function jumpOnBoard(player){
-		for (var i = 0; i<JDICE; i++){
-			if(start>=1 && start<11){
-				if(start==1){
+
+	function jumpOnBoard(player) {
+		for(var i = 0; i < JDICE; i++) {
+			if(start >= 1 && start < 11) {
+				if(start == 1) {
 					playerLeftJump(player, LEFT_BIG_JUMP);
-				}else if(start==10){
+				} else if(start == 10) {
 					playerLeftJump(player, LEFT_BIG_JUMP);
-				}else{
+				} else {
 					playerLeftJump(player, LEFT_SMALL_JUMP);
 				}
-			}else if(start>=11 && start<21){
-				if(start==11){
+			} else if(start >= 11 && start < 21) {
+				if(start == 11) {
 					playerTopJump(player, TOP_BIG_JUMP);
-				}else if(start==20){
+				} else if(start == 20) {
 					playerTopJump(player, TOP_BIG_JUMP);
-				}else{
+				} else {
 					playerTopJump(player, TOP_SMALL_JUMP);
 				}
 
-			}else if(start>=21 && start<31){
-				if(start==21){
+			} else if(start >= 21 && start < 31) {
+				if(start == 21) {
 					playerLeftJump(player, RIGHT_BIG_JUMP);
-				}else if(start==30){
+				} else if(start == 30) {
 					playerLeftJump(player, RIGHT_BIG_JUMP);
-				}else{
+				} else {
 					playerLeftJump(player, RIGHT_SMALL_JUMP);
 				}
-			}else{
-				if(start==31){
+			} else {
+				if(start == 31) {
 					playerTopJump(player, DOWN_BIG_JUMP);
-				}else if(start==40){
+				} else if(start == 40) {
 					playerTopJump(player, DOWN_BIG_JUMP);
-				}else{
+				} else {
 					playerTopJump(player, DOWN_SMALL_JUMP);
 				}
 			}
 
-		start++;
-		if(start>40)
-		start-=40;
+			start++;
+			if(start > 40) start -= 40;
 		}
 	}
-	
+
 	/* Method of choosing the player */
-	function choosePlayer(playerColor){
-	if(playerColor== COLOR_PLAYER_1){
-		stepOnBoard(player1);
+
+	function choosePlayer(playerColor) {
+		if(playerColor == COLOR_PLAYER_1) {
+			stepOnBoard(player1);
+		} else if(playerColor == COLOR_PLAYER_2) {
+			stepOnBoard(player2);
+		} else if(playerColor == COLOR_PLAYER_3) {
+			stepOnBoard(player3);
+		} else {
+			stepOnBoard(player4);
+		}
 	}
-	else if(playerColor == COLOR_PLAYER_2){
-		stepOnBoard(player2);
-	}else if(playerColor == COLOR_PLAYER_3){
-		stepOnBoard(player3);
-	}else{
-		stepOnBoard(player4);
-	}
-	}
-	
-	/*
-	jumpOnBoard(PLAYER_2);
-	*/
-	
 
 	/*
-	stepOnBoard(player1);
-	stepOnBoard(PLAYER_2);
-	stepOnBoard(player3);
-	stepOnBoard(player4);
-	*/
+	 * jumpOnBoard(PLAYER_2);
+	 */
 
-	
+	/*
+	 * stepOnBoard(player1); stepOnBoard(PLAYER_2);
+	 * stepOnBoard(player3); stepOnBoard(player4);
+	 */
 
+	/* Change the owner of the city!! */
 
-/* Change the owner of the city!!*/
+	/*
+	 * var ownerCell = '#ownerCell2';
+	 *
+	 * $('#ownerCell2').css('backgroundColor',player1);
+	 *
+	 * var cellColor = function(Cell){ var t =
+	 * $(Cell).css('backgroundColor'); }; /* /* var tt =
+	 * cellColor(ownerCell); alert(tt);
+	 */
+	/*
+	 * function(){ if(player==cellColor(ownerCell)){ alert("!"); }else
+	 * if{ alert("1"); } };
+	 */
+	/*
+	 * $('#ownerCell6').css('backgroundColor',cellColor(ownerCell));
+	 */
 
-/*
-var ownerCell = '#ownerCell2';
-
-$('#ownerCell2').css('backgroundColor',player1);
-
-var cellColor = function(Cell){
-var t = $(Cell).css('backgroundColor');
-
-};
-/*
-
-/*
-var tt = cellColor(ownerCell);
-alert(tt);
-*/
-/*
-function(){
-if(player==cellColor(ownerCell)){
-alert("!");
-}else if{
-alert("1");
-}
-};
-*/
-/*
-$('#ownerCell6').css('backgroundColor',cellColor(ownerCell));
-*/
-
-/*
-var changeOwner = function (player, Cell){
-if(player==cellColor(ownerCell)){
-alert("It's you city!");
-}else if((cellColor(Cell)==defaultColor){
-alert("You can buy!");
-}else{
-alert("Pay the tax!");
-}
-};
-changeOwner(player1, ownerCell);
-*/
-
+	/*
+	 * var changeOwner = function (player, Cell){
+	 * if(player==cellColor(ownerCell)){ alert("It's you city!"); }else
+	 * if((cellColor(Cell)==defaultColor){ alert("You can buy!"); }else{
+	 * alert("Pay the tax!"); } }; changeOwner(player1, ownerCell);
+	 */
 
 	/*--Accardion--*/
 	$(function() {
-			$( "#accordion" ).accordion();
-		});
-		
-		/*--BUTTON--*/
+		$("#accordion").accordion();
+	});
+
+	/*--BUTTON--*/
 	$(function() {
-        $( "input[type=submit]" )
-            .button()
-            .click(function( event ) {
-                event.preventDefault();
-            });
-    });
-
-	
-	
-	/**** ROLL DICE ****/
-	function roolDice(dice1, dice2, player){
-	$("#diceButton").click(function(){
-		$("#diceImg1").attr("src", "resources/img/board/die"+dice1+".gif");
-		$("#diceImg2").attr("src", "resources/img/board/die"+dice2+".gif");
-		
-		choosePlayer(player);
-	
+		$("input[type=submit]").button().click(function(event) {
+			event.preventDefault();
+		});
 	});
+
+	/** ** ROLL DICE *** */
+
+	function roolDice(dice1, dice2, player) {
+		$("#diceButton").click(
+
+		function() {
+			$("#diceImg1").attr("src", "resources/img/board/die" + dice1 + ".gif");
+			$("#diceImg2").attr("src", "resources/img/board/die" + dice2 + ".gif");
+
+			choosePlayer(player);
+
+		});
 	};
-	
+
 	roolDice(DICE1, DICE2, COLOR_PLAYER_1);
-	
-	/**** BUY ****/
-	$(function(){
-	$("#buyButton").click(function(){
-	$("#ownerCell"+(DICE+1)+"").css('backgroundColor', COLOR_PLAYER_1);
-	
-	});
-	});
-	
-	
- 
- 
- /*
-  $("#minicell").mouseover(function() {
-        $(this).addClass("ui-state-active");
-    });
-    $("#minicell").mouseout(function() {
-        $(this).removeClass("ui-state-active");
-    });
-    $('#minicell').click(function(event) {
-        $(this).toggleClass('selectRow'); 
-    });
- */
 
-});
+	/** ** BUY *** */
+	$(function() {
+		$("#buyButton").click(
 
+		function() {
+			$("#ownerCell" + (DICE + 1) + "").css('backgroundColor', COLOR_PLAYER_1);
+
+		});
+	});
+
+	/*
+	 * $("#minicell").mouseover(function() {
+	 * $(this).addClass("ui-state-active"); });
+	 * $("#minicell").mouseout(function() {
+	 * $(this).removeClass("ui-state-active"); });
+	 * $('#minicell').click(function(event) {
+	 * $(this).toggleClass('selectRow'); });
+	 */
+
+	// });
 	
--- src/main/webapp/resources/js/monopoly/monopoly.js
+++ src/main/webapp/resources/js/monopoly/monopoly.js
@@ -3,219 +3,232 @@ console, alert, document,
 setInterval, clearInterval,
 prompt, bootbox*/
 
-// $(document).ready(function() {
-"use strict";
-var chat = {
-	$chat: null,
-	init: function() {
-		console.log('Initing chat');
-		chat.$chat = $('#chat');
-	},
-	append: function(what) {
-		chat.$chat.prepend('<li>' + what + '</li>');
-	}
-};
+$(document).ready(function() {
+	"use strict";
+	var chat = {},
+		MONO = {};
 
-var MONO = {};
+	chat = {
+		$chat: null,
+		init: function() {
+			console.log('Initing chat');
+			chat.$chat = $('#chat');
+		},
+		append: function(what) {
+			chat.$chat.prepend('<li>' + what + '</li>');
+		}
+	};
 
-MONO = {
-	config: {
-		idGame: null,
-		idUser: null,
-		socketUrl: "ws://localhost:8888/",
-		ajaxUrl: "mono-ajax",
-		isCreator: false,
-		money: 0,
-		color: '',
-		game_status: ""
-	},
-	transport: {
-		socket: null,
-		send: function(type, json) {
-			var jsonString = JSON.stringify({
-				'type': type,
-				'id': {
-					'game-id': MONO.config.idGame,
-					'user-id': MONO.config.idUser
+	MONO = {
+		config: {
+			idGame: null,
+			idUser: null,
+			socketUrl: "ws://localhost:8888/",
+			ajaxUrl: "mono-ajax",
+			isCreator: false,
+			money: 0,
+			color: '',
+			game_status: ""
+		},
+		transport: {
+			socket: null,
+			send: function(type, json) {
+				var jsonString = JSON.stringify({
+					'type': type,
+					'id': {
+						'game-id': MONO.config.idGame,
+						'user-id': MONO.config.idUser
+					},
+					'data': json
+				});
+				console.log('sending message: ' + jsonString);
+				MONO.transport.socket.send(jsonString);
+			},
+			init: function() {
+
+				$.ajax({
+					url: MONO.config.ajaxUrl,
+					type: 'post',
+					success: function(init_id) {
+						console.log('Inner id received');
+						console.log('my idUser is: ' + init_id.idUser);
+						console.log('my idGame is: ' + init_id.idGame);
+
+						MONO.config.idUser = init_id.idUser;
+						MONO.config.idGame = init_id.idGame;
+
+						console.log('Initing web-sockets');
+						MONO.transport.socket = new WebSocket(MONO.config.socketUrl);
+						MONO.transport.socket.onopen = MONO.events.onConnectEstablished;
+						MONO.transport.socket.onclose = MONO.events.onConnectClosed;
+						MONO.transport.socket.onmessage = MONO.events.onMessage;
+					}
+				});
+			}
+		},
+
+		events: {
+			onConnectEstablished: function() {
+				console.log("Connection opened, bind-websocket request");
+				MONO.transport.send('bind-websocket', {});
+				MONO.transport.send('init', {});
+			},
+			onConnectClosed: function() {
+				console.log("Connection closed");
+			},
+			onMessage: function(event) {
+				console.log("Received message: ", event.data);
+				var json = JSON.parse(event.data);
+				if(typeof MONO.events.handle[json.type] === 'undefined') {
+					console.error('No handlers for ' + json.type + ' defined');
+				} else {
+					MONO.events.handle[json.type](json);
+				}
+			},
+			handle: {
+				'undefined': function(json) {
+					console.log('Unknown response');
 				},
-				'data': json
-			});
-			console.log('sending message: ' + jsonString);
-			MONO.transport.socket.send(jsonString);
+				'roll': function(json) {
+					console.log('[roll] event');
+					var dice1 = json.dice1,
+						dice2 = json.dice2,
+						offset = parseInt(dice1, 10) + parseInt(dice2, 10),
+						buttons = json.game_state.buttons,
+						color = json.game_state.player,
+						money = json.game_state.player_money,
+						message = json.game_state.messages;
+					console.log('Current money is: ' + money);
+					MONO.config.money = money;
+
+					MONO.animate.move(color, dice1, dice2);
+
+					// this player moves
+					if(MONO.config.color === color) {
+						$("#controls .btn").each(function(i, btn) {
+							$(btn).animate({
+								"opacity": "0.5"
+							});
+						});
+
+						MONO.animate.refresh_buttons(buttons);
+						if(message) {
+							chat.append("server: " + message);
+						}
+					}
+					chat.append("player[" + color + "] is on cell: " + json.cell.name);
+
+				},
+				'init': function(json) {
+					console.log('[init] event');
+					MONO.config.color = json.color;
+					MONO.config.money = json.money;
+					MONO.config.isCreator = json.isCreator;
+
+					if(MONO.config.isCreator) {
+						$('#start').show(100);
+					} else {
+						$('#start').hide(100);
+					}
+				},
+				'chat': function(json) {
+					chat.append(json.message);
+				},
+				'logic': function(json) {
+					alert('game-status:' + json.game_status);
+					MONO.config.game_status = json.game_status;
+					if(json.game_status === "start") {
+						$('#ready').hide(100);
+						$('#start').hide(100);
+					}
+				},
+				'ready': function(json) {
+					chat.append(json.player + ' is ready=' + json.ready);
+				},
+				'turn': function(json) {
+					chat.append(json.player + ' turn=true');
+					$('.btn').animate({
+						"opacity": "0.5"
+					}, 100);
+					if(json.player === MONO.config.color) {
+						$('#roll').animate({
+							"opacity": "1"
+						}, 100);
+					}
+				}
+			}
+		},
+		animate: {
+			refresh_buttons: function(buttons) {
+				$.each(buttons, function(btnName) {
+					var opacityValue = buttons[btnName] ? "1" : "0.5";
+					$('#' + btnName).animate({
+						"opacity": opacityValue
+					}, 100);
+				});
+			},
+			move: function(who, d1, d2) {
+				chat.append('Moving ' + who + ' to offset: ' + (d1 + d2));
+				// DICE = d1 + d2;
+
+				// stepOnBoard(who === "RED" ? '#player1' : '#player2');
+
+
+				console.log('Moving to: ' + (d1 + d2));
+			}
 		},
 		init: function() {
+			MONO.transport.init();
+			chat.init();
 
-			$.ajax({
-				url: MONO.config.ajaxUrl,
-				type: 'post',
-				success: function(init_id) {
-					console.log('Inner id received');
-					console.log('my idUser is: ' + init_id.idUser);
-					console.log('my idGame is: ' + init_id.idGame);
-
-					MONO.config.idUser = init_id.idUser;
-					MONO.config.idGame = init_id.idGame;
-
-					console.log('Initing web-sockets');
-					MONO.transport.socket = new WebSocket(MONO.config.socketUrl);
-					MONO.transport.socket.onopen = MONO.events.onConnectEstablished;
-					MONO.transport.socket.onclose = MONO.events.onConnectClosed;
-					MONO.transport.socket.onmessage = MONO.events.onMessage;
-				}
+			$('#roll').click(function() {
+				console.log('sending **roll** message to server');
+				MONO.transport.send('roll', {});
 			});
-		}
-	},
+			$('#buy').click(function() {
+				console.log('sending **buy** message to server');
+				MONO.transport.send('buy', {});
+			});
+			$('#pay').click(function() {
+				console.log('sending **pay** message to server');
+				MONO.transport.send('pay', {});
+			});
 
-	events: {
-		onConnectEstablished: function() {
-			console.log("Connection opened, bind-websocket request");
-			MONO.transport.send('bind-websocket', {});
-			MONO.transport.send('init', {});
-		},
-		onConnectClosed: function() {
-			console.log("Connection closed");
-		},
-		onMessage: function(event) {
-			console.log("Received message: ", event.data);
-			var json = JSON.parse(event.data);
-			if(typeof MONO.events.handle[json.type] === 'undefined') {
-				console.error('No handlers for ' + json.type + ' defined');
-			} else {
-				MONO.events.handle[json.type](json);
-			}
-		},
-		handle: {
-			'undefined': function(json) {
-				console.log('Unknown response');
-			},
-			'roll': function(json) {
-				console.log('[roll] event');
-				var dice1 = json.dice1,
-					dice2 = json.dice2,
-					offset = parseInt(dice1, 10) + parseInt(dice2, 10),
-					buttons = json.buttons,
-					color = json.player,
-					money = json.money;
-				console.log('Current money is: ' + money);
-				MONO.config.money = money;
-
-				MONO.animate.move(color, dice1, dice2);
-
-				// this player moves
-				if(MONO.config.color === color) {
-					$("#controls .btn").each(function(i, btn) {
-						$(btn).animate({
-							"opacity": "0.5"
-						});
-					});
-
-					MONO.animate.refresh_buttons(buttons);
-					// $.each(buttons, function(btnName) {
-					// var opacityValue = buttons[btnName] ? "1" : "0.5";
-					//$('#' + btnName).animate({
-					//"opacity": opacityValue
-					//}, 100);
-					//});
-					if(buttons.message) {
-						chat.append("server: " + buttons.message);
-					}
-					chat.append("player["+ color + "] on cell: " + json.cell.name);
-				}
-
-			},
-			'init': function(json) {
-				console.log('[init] event');
-				MONO.config.color = json.color;
-				MONO.config.money = json.money;
-				MONO.config.isCreator = json.isCreator;
-
-				if(MONO.config.isCreator) {
-					$('#start').show(100);
-				} else {
-					$('#start').hide(100);
-				}
-			},
-			'chat': function(json) {
-				chat.append(json.message);
-			},
-			'logic': function(json) {
-				alert('game-status:' + json.game_status);
-				MONO.config.game_status = json.game_status;
-				if(json.game_status === "start") {
-					$('#ready').hide(100);
-					$('#start').hide(100);
-				}
-			},
-			'ready': function(json) {
-				chat.append(json.player + ' is ready=' + json.ready);
-			},
-			'turn': function(json) {
-				chat.append(json.player + ' turn=true');
-				$('.btn').animate({
+			$('#done').click(function() {
+				console.log('sending ***done*** message to server');
+				$(".btn").animate({
 					"opacity": "0.5"
-				}, 100);
+				});
 
-				if(json.player === MONO.config.color) {
-					$('#roll').animate({
-						"opacity": "1"
-					}, 100);
-				}
-			}
-		}
-	},
-	animate: {
-		refresh_buttons: function(buttons) {
-			$.each(buttons, function(btnName) {
-				var opacityValue = buttons[btnName] ? "1" : "0.5";
-				$('#' + btnName).animate({
-					"opacity": opacityValue
-				}, 100);
-			});
-		},
-		move: function(who, d1, d2) {
-			chat.append('Moving ' + who +' to offset: ' + (d1 + d2));
-			console.log('Moving to: ' + (d1 + d2));
-		}
-	},
-	init: function() {
-		MONO.transport.init();
-		chat.init();
-
-		$('#roll').click(function() {
-			console.log('sending **roll** message to server');
-			MONO.transport.send('roll', {});
-		});
-		$('#buy').click(function() {
-			console.log('sending **buy** message to server');
-			MONO.transport.send('buy', {});
-		});
-		$('#pay').click(function() {
-			console.log('sending **pay** message to server');
-			MONO.transport.send('pay', {});
-		});
-		$('#ok').click(function() {
-			console.log('sending **ok** message to server');
-			MONO.transport.send('ok', {});
-		});
-		$('#done').click(function() {
-			console.log('sending ***done*** message to server');
-			$(".btn").animate({
-				"opacity": "0.5"
+				MONO.transport.send('done', {});
 			});
 
-			MONO.transport.send('done', {});
-		});
+			$('#mortage').click(function() {
+				var pos = parseInt(prompt('Enter position to mortage'), 10);
+				console.log('sending ***mortage*** message to server');
+				MONO.transport.send('mortage', {
+					position: pos
+				});
+			});
 
-		$('#start').click(function() {
-			console.log('sending ***start*** message to server');
-			MONO.transport.send('start', {});
-		});
+			$('#unmortage').click(function() {
+				var pos = parseInt(prompt('Enter position to unmortage'), 10);
+				console.log('sending ***unmortage*** message to server');
+				MONO.transport.send('unmortage', {
+					position: pos
+				});
+			});
 
-		$('#ready').click(function() {
-			console.log('sending ***ready*** message to server');
-			MONO.transport.send('ready', {});
-		});
-	}
-};
-MONO.init();
-// });
+			$('#start').click(function() {
+				console.log('sending ***start*** message to server');
+				MONO.transport.send('start', {});
+			});
+
+			$('#ready').click(function() {
+				console.log('sending ***ready*** message to server');
+				MONO.transport.send('ready', {});
+			});
+		}
+	};
+	MONO.init();
+});
