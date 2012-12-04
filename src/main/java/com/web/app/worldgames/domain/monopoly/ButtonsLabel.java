1aeea77ab6c3e61a6a69b7a247fa423527bbd1d8
LittleInn <InnaPetryshyn@gmail.com>
2012-12-04 23:17:45 +0200

    buttons action

--- src/main/java/com/web/app/worldgames/domain/monopoly/ButtonsLabel.java
+++ src/main/java/com/web/app/worldgames/domain/monopoly/ButtonsLabel.java
@@ -2,21 +2,19 @@ package com.web.app.worldgames.domain.monopoly;
 
 public interface ButtonsLabel {
 	String ROLL = "roll";
-	String OK = "ok";
 	String BUY = "buy";
 	String BUILD = "build";
-	String REFUSE = "refuse";
 	String PAY = "pay";
 	String MORTAGE = "mortage";
 	String UNMORTAGE = "unmortage";
 	String SELL = "sell";
 	String START = "start";
+	String AUCTION = "auction";
 	String READY = "ready";
-	String NEXT = "next";
 	String DONE = "done";
+	
 	String TURN = "turn";
 	String INIT = "init";
-	String AUCTION = "auction";
 	String LOGIC = "logic";
 	String CHAT = "chat";
 	String CONNECT = "connect";
---
+++ src/main/java/com/web/app/worldgames/domain/monopoly/game/ButtonsAction.java
@@ -0,0 +1,19 @@
+package com.web.app.worldgames.domain.monopoly.game;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.web.app.worldgames.domain.monopoly.ButtonsLabel;
+import com.web.app.worldgames.domain.monopoly.Player;
+
+public class ButtonsAction {
+	static Map<String, Object> buttons = new HashMap<String, Object>();
+public static Map<String, Object> buttonsAction(Player player){
+	buttons.put(ButtonsLabel.BUILD, player.canBuild());
+	buttons.put(ButtonsLabel.MORTAGE, player.canMortage());
+	buttons.put(ButtonsLabel.ROLL, player.canRollDices());
+	buttons.put(ButtonsLabel.SELL, player.canSell());
+	buttons.put(ButtonsLabel.UNMORTAGE, player.canUnmortage());
+	return buttons;
+}
+}
